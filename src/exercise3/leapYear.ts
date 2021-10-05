93e58110302bc28c2a4db714506f9c1734192960
Lee Sanderson <lee.sndrsn@gmail.com>
2021-10-05 14:05:14 +0100

    Refactor to use explainatory methods

--- src/exercise3/leapYear.ts
+++ src/exercise3/leapYear.ts
@@ -1,6 +1,22 @@
 
 export class LeapYear {
+    
     public static isLeapYear(year: number): boolean {
-        return year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0);
+        return new LeapYear(year).leapYear();
+    }
+
+    private constructor(private year: number){
+    }
+
+    leapYear(): boolean {
+        return this.divisibleBy(4) && !(this.divisibleBy(100) && this.notDivisibleBy(400));
+    }
+
+    private notDivisibleBy(number: number) {
+        return this.year % number != 0;
+    }
+
+    private divisibleBy(number: number) {
+        return this.year % number == 0;
     }
 }
